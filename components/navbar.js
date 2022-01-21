e6e97a500fd2d21ddc348935a49854416b7d0841
Sarsande-Sunil <93374701+Sarsande-Sunil@users.noreply.github.com>
2022-01-21 12:21:37 +0530

    Project Done
    This project done by me Individually

---
+++ components/navbar.js
@@ -0,0 +1,169 @@
+function navbar() {
+
+    return `
+  <!-- navbar -->
+  <div class="top-box">
+    <a href="home.html"><p>FREE SHIPPING ON ALL ORDERS OVER $35!</p></a>
+  </div>
+  <div class="navbar">
+    <!-- logo -->
+  
+    <div class="list-l">
+      <ul>
+
+      
+    <a href="https://himalayausa.com/">
+    <img class="image"
+      src="https://cdn.shopify.com/s/files/1/0272/4714/9155/files/90_Plus_years_300px.png?height=628&pad_color=ffffff&v=1622119115&width=1200"
+      alt="1"
+    />
+    </a>
+       
+      
+       <a href="product.html" class="li-text"> <li>Shop</li></a>
+        <a href="https://himalayausa.com/pages/about" class="li-text"><li>About</li></a>
+        <a href="https://himalayausa.com/pages/store-locator" class="li-text"><li>Store Locator</li></a>
+       <a href="">
+          <li class="img-search">
+              <img
+                src="https://www.seekpng.com/png/detail/12-123945_search-simple-instagram-search-icon-free.png"
+                alt=""
+              />
+            </li>
+       </a>
+        <a href="signup.html">
+          <li class="account">
+              <img
+                src="https://www.pngkit.com/png/detail/281-2812821_user-account-management-logo-user-icon-png.png"
+                alt="1"
+              />
+            </li>
+        </a>
+       <a href="chekout.html">
+          <li class="cart">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOpHS_JIkkmUGee4XaHtybE7f_IIZBqDRlgCQXHGdE1UCDj90d-3eClRGQ0dHxAotL9QQ&usqp=CAU"
+                alt="1"
+              />
+            </li>
+       </a>
+      </ul>
+    </div>
+ 
+  </div>`  
+}
+
+function footerreturn() {
+
+
+  return `  <div id="bigbox">
+  <div class="box8">
+      <!-- box8-1 -->
+      <div class="box8-1">
+          <h1 class="box8-h1">info</h1>
+          <a href="https://himalayausa.com/pages/contact-us"> 
+           <h2 class="box8-h2">contact us</h2>
+          </a>
+          <a href="https://himalayausa.com/pages/faq"> 
+              <h2 class="box8-h2">FAQ</h2>
+          </a>
+          <a href="https://himalayausa.com/blogs/health"> 
+           <h2 class="box8-h2">BLOG</h2>
+          </a>
+          <a href="https://himalayausa.com/pages/store-locator"> 
+           <h2 class="box8-h2">Store Locator</h2>
+          </a>
+          <a href="https://himalayausa.com/pages/careers"> 
+           <h2 class="box8-h2">Careeres</h2>
+          </a>
+      </div>
+       <!-- box8-2 -->
+       <div class="box8-1">
+           <h1 class="box8-h1">Policies</h1>
+          <a href="https://himalayausa.com/pages/shipping-returns"> 
+           <h2 class="box8-h2">SHIPPING & RETURNS</h2>
+          </a>
+          <a href="https://himalayausa.com/pages/terms-of-use"> 
+              <h2 class="box8-h2">TERMS AND CONDITIONS</h2>
+          </a>
+          <a href="https://himalayausa.com/pages/privacy-policy"> 
+           <h2 class="box8-h2">Privacy Policy</h2>
+          </a>
+          <a href="https://himalayausa.com/pages/privacy-notice-for-california-residents"> 
+           <h2 class="box8-h2">CCPA</h2>
+          </a>
+          <a href="https://himalayausa.com/pages/website-accessibility-policy"> 
+           <h2 class="box8-h2">Accessibility </h2>
+          </a>
+       </div>
+        <!-- box8-3 -->
+      <div class="box8-1">
+          <a href="">
+              <h1 class="box8-1-h1">Contact Us</h1>
+          </a>
+    
+          <a href="">
+           <h2 class="box8-1-h2">Email:</h2>
+           </a>
+          <a href="">
+           <h3 class="box8-1-h3">writetous@himalayausa.com</h3>
+          </a>
+    
+          <a href="">
+           <h2 class="box8-1-h2">Phone:</h2>
+           </a>
+          <a href="">
+           <h3 class="box8-1-h3">
+               1-800-869-4640
+               Mon-Fri, </h3>
+          </a>
+          <a href="">
+           <h3 class="box8-1-h3">Mon-Fri, 8am-5pm CT</h3>
+          </a>
+    
+          <a href="">
+           <h2 class="box8-1-h2">Mail:</h2>
+           </a>
+          <a href="">
+           <h3 class="box8-1-h3">
+               Himalaya Wellness
+               1101 Gillingham Lane
+               Sugar Land, TX 77478​
+           </h3>
+          </a>     
+      </div>
+       <!-- box8-4 -->
+       <div class="box8-1">
+           <a href="">
+               <h1 class="box8-1-h1">Subscribe to Our Newsletter!</h1>
+           </a>
+           <input type="text" placeholder="Enter Your Email" class="box8-input">
+    
+           <a href="">
+               <h3 class="box8-1-h3">
+                   By entering your email, 
+                   you agree to our Terms & 
+                   Conditions and Privacy Policy.
+               </h3>
+           </a>
+       </div>
+    </div>
+    <!-- box9 -->
+    <div class="box9">
+    <h4 class="box9-h4">© 2022 Himalaya Wellness. All rights reserved.</h4>
+    </div>
+    <!-- box10 -->
+    <div class="box10">
+       <h4 class="box9-h4">
+           *THESE STATEMENTS HAVE NOT BEEN EVALUATED 
+           BY THE FOOD AND DRUG ADMINISTRATION. 
+           THIS PRODUCT IS NOT INTENDED TO DIAGNOSE,
+            TREAT, CURE OR PREVENT ANY DISEASE.
+       </h4>  
+    </div>
+</div>
+`
+ 
+}
+
+export {navbar,footerreturn};
